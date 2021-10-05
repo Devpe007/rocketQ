0978c911daddc8755e713f1245efba99efd5cb70
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-10-05 13:49:32 -0300

    moving all html files to the views folder and turning them into ejs

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,8 @@ const routes = Router();
 
 routes.get('/', (request, response) => response.render('home'));
 
+routes.get('/room', (request, response) => response.render('room'));
+
+routes.get('/create-pass', (request, response) => response.render('create-pass'));
+
 module.exports = routes;
