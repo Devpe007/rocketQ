f1673f7ca1c19e8081446a1056b291e8f8db7a09
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-10-05 19:12:34 -0300

    setting config middleware in project

--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ server.use(express.static('public'));
 
 server.set('views', path.join(__dirname, 'views'));
 
+server.use(express.urlencoded({ extended: true }));
+
 server.use(routes);
 
 server.listen(3000, () => console.log('Server started on port 3000'));
