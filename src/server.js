9d0d7e5d4e36e81da0c3bc81ad19958b9c323670
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-10-05 13:39:17 -0300

    setting server settings to run ejs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const path = require('path');
+
+const routes = require('./routes');
 
 const server = express();
+
+server.set('view engine', 'ejs');
+
+server.use(express.static('public'));
+
+server.set('views', path.join(__dirname, 'views'));
+
+server.use(routes);
 
 server.listen(3000, () => console.log('Server started on port 3000'));
