4aa2a347574cd3fd59456cf123350f352083ab68
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-10-06 14:34:48 -0300

    create create method in question-controller

--- src/controllers/question-controller.js
+++ src/controllers/question-controller.js
@@ -1,3 +1,5 @@
+const Database = require('../database/config');
+
 class QuestionController {
   index(request, response) {
     const { room_id, question_id, action } = request.params;
@@ -9,6 +11,25 @@ class QuestionController {
         action = ${action}, 
         password = ${password}
     `);
+  };
+
+  async create(request, response) {
+    const db = await Database();
+
+    const { question } = request.body;
+    const { room_id } = request.params;
+
+    db.run(`INSERT INTO questions (
+        title,
+        read,
+        room
+    ) VALUES (
+        "${question}",
+        0,
+        ${room_id}
+    )`);
+
+    response.redirect(`/room/${room_id}`);
   };
 };
 
