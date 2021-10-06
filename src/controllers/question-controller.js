ae2eed4aa080becb8becadb51ff641bb19ac04c4
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-10-06 19:16:16 -0300

    create the functionality to delete and mark as read a question

--- src/controllers/question-controller.js
+++ src/controllers/question-controller.js
@@ -1,16 +1,23 @@
 const Database = require('../database/config');
 
 class QuestionController {
-  index(request, response) {
+  async index(request, response) {
+    const db = await Database();
+
     const { room_id, question_id, action } = request.params;
     const { password } = request.body;
 
-    console.log(`
-        room = ${room_id}, 
-        questionId = ${question_id}, 
-        action = ${action}, 
-        password = ${password}
-    `);
+    // Verificar se a senha esta correta;
+    const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${room_id}`);
+    if (verifyRoom.password === password) {
+      if (action === 'delete') {
+        await db.run(`DELETE FROM questions WHERE id = ${question_id}`);
+      } else if (action === 'check') {
+        await db.run(`UPDATE questions SET read = 1 WHERE id = ${question_id}`);
+      };
+    };
+
+    response.redirect(`/room/${room_id}`);
   };
 
   async create(request, response) {
